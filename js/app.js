aee8b74d424b48370e1172138c1e6607be01e821
javierbyte <hola@javierbyte.com>
2014-04-09 05:52:33 -0600

    working ui

--- js/app.js
+++ js/app.js
@@ -2,40 +2,41 @@
 var $body = $('body');
 var $fade = $('#fade');
 
-/* starting */
-
-var currentSection = $('section')[0];
-var currentArticle = $(currentSection).find('article')[0];
-
-//showing inly the first article in the section
-$('section').each(function() {
-	$(this).find('article').first().addClass('active');
-});
-
-$('#side a').on('click', function(event) {
-
-});
-
-
 /*
  * UI FUNCTIONS
  */
 
 /* primitive functions */
-function loadArticle(article) {
-	$(currentArticle).removeClass('active');
-	currentArticle = $(article)[0];
-	$(currentArticle).addClass('active');
+
+function load(section,article) {
+	if(section && section != currentSection) {
+		if(section[0] != '#') section = '#' + section;
+		$(currentSection).addClass('fade');
+		currentSection = section;
+		currentArticle = '#' + $(currentSection + ' article').first().attr('id');
+		if(!article) article = currentArticle; // if there is no specific article set, then go with the 1st
+		$(currentSection).addClass('active');
+	}
+
+	if(article) {
+		if(article[0] != '#') article = '#' + article;
+		$(currentSection + ' .active').removeClass('active');
+		currentArticle = article;
+		$(article).addClass('active');
+
+		if( $(currentArticle).attr('data-title') ) {
+			$(currentSection + ' .title').html( $(currentArticle).attr('data-title') );
+		}
+	}
+
+	var moving = setTimeout(function() {
+		$('.fade').removeClass('active fade');
+	},500);
 }
 
 function closeSide() {
 	$body.removeClass('showSide');
 }
-
-function titleSection(title) {
-	$(currentSection).find('.title').html(title);
-}
-
 
 /* ui functions */
 $('*[open-nav]').on('click', function() {
@@ -43,11 +44,25 @@ $('*[open-nav]').on('click', function() {
 	$body.addClass('showSide');
 });
 
+$('*[data-section]').on('click', function() {
+	event.preventDefault();
+	load($(this).attr('data-section'),0);
+	closeSide();
+})
+
 $('*[data-article]').on('click', function() {
 	event.preventDefault();
-	loadArticle($(this).attr('data-article'));
-	titleSection($(this).html());
+	load(0,$(this).attr('data-article'));
 	closeSide();
 })
 
 $fade.on('click', closeSide);
+
+/* starting */
+var currentSection = '#' + $('section').first().attr('id');
+var currentArticle = '#' + $(currentSection + ' article').first().attr('id');
+
+$(currentSection).addClass('active');
+$(currentArticle).addClass('active');
+
+var history = [];
